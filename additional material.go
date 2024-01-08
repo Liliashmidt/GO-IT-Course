f179dbaf135092c111fac7cef3a0015666f47ac6
Lilia Shmidt <156001207+Liliashmidt@users.noreply.github.com>
2024-01-08 19:12:25 +0200

    Create additional material.go

---
+++ additional material.go
@@ -0,0 +1,18 @@
+// import (
+// 	"bytes"
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"testing"
+
+// 	"github.com/stretchr/testify/assert"
+
+// 	testutil "github.com/Pylons-tech/pylons/testutil/cli"
+// )
+
+var problemSpecificationsDir string
+
+// exerciseDir is the location of the exercise and also the cases_test.go file.
+// This assumes that the generator script lives in the .meta directory within
+// the exercise directory. Falls back to the present working directory.
+var exerciseDir string
